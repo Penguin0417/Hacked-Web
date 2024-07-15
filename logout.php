9b8f73a917e030b54eb59d54d8384f6bf01ca8a1
Penguin0417 <167802327+Penguin0417@users.noreply.github.com>
2024-07-15 14:20:17 +0530

    Update logout.php

---
+++ logout.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style/logout.css">
+    <title>Login Page</title>
+</head>
+<body>
+    <header>
+        
+        <h1>Student Management System</h1>
+    </header>
+    <br><br>
+    <div class="login-container">
+
+      
+        <h1>Logout your account</h1>
+        <img src="C:\Users\cuj\Downloads\images.jpg">
+        <button onclick="location.href='login.php'">Logout</button>
+       
+    </div>
+    <footer>
+        <p>&copy; Student Management System</p>
+    </footer>
+</body>
+</html>
